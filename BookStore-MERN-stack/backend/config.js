170553218dea393c58a1743b23a713e97bddd8d9
DonLemos <donovangarethlemos@gmail.com>
2024-07-13 18:49:22 +0200

    update

---
+++ BookStore-MERN-stack/backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://donovangarethlemos:09151DgL!@books-store-mern.rngse0t.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN'
