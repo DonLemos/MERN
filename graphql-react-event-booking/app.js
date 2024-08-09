52f63a2d82d928413f42361dab4183da7b54e040
DonLemos <donovangarethlemos@gmail.com>
2024-08-09 16:03:00 +0200

    Update

--- graphql-react-event-booking/app.js
+++ graphql-react-event-booking/app.js
@@ -12,6 +12,31 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const events = eventIds => {
+    return Event.find({ _id: {$in: eventIds } })
+    .then(events => {
+        return events.map(event => {
+            return { 
+                ...event._doc, 
+                _id: event.id, 
+                creator: user.bind(this, event.creator) };
+        });
+    })
+    .catch(err => {
+        throw err;
+    });
+};
+
+const user = userId => {
+    return User.findById(userId)
+    .then(user => {
+        return { ...user._doc, _id: user.id, createdEvents: events.bind(this, user._doc.createdEvents) };
+    })
+    .catch(err => {
+        throw err;
+    });
+};
+
 app.use(
   "/graphql",
   graphqlHTTP({
@@ -22,12 +47,14 @@ app.use(
         description: String!
         price: Float!
         date: String!
+        creator: User!
         }
 
         type User {
         _id: ID!
         email: String!
         password: String
+        createdEvents: [Event!]
         }
 
         input EventInput {
@@ -59,9 +86,10 @@ app.use(
     rootValue: {
       events: () => {
         return Event.find()
+        .populate('creator')
           .then((events) => {
-            return events.map((event) => {
-              return { ...event._doc, _id: event.id };
+            return events.map(event => {
+              return { ...event._doc, _id: event.id, creator: user.bind(this, event._doc.creator) };
             });
           })
           .catch((err) => {
